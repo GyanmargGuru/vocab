fcdb99c9d7370d6d5f5654ff8cbcab3248f262b3
GyanmargGuru <147705168+GyanmargGuru@users.noreply.github.com>
2023-10-12 18:44:12 +0530

    Create head.php

---
+++ head.php
@@ -0,0 +1,46 @@
+<head>
+    <meta charset="UTF-8" />
+    <title>Vocab</title>
+    <meta name="description" content="Path of knowledge Vocabulary" />
+    <meta name="keywords" content="guru spiritual path practice knowledge yoga kundalini occult seeking liberation गुरु बुद्धि आध्यात्मिक मार्ग पथ साधना ज्ञान योग कुंडलिनी तंत्र मुक्ति" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    
+<style>
+
+		body {
+			background-image: url('images/gmbk03.jpg');
+			background-repeat: repeat-y;
+			background-size: 100% 100%;
+		}
+      
+		hr {
+			height: 1px;
+			color: #ccc;
+			background-color: #ccc;
+			border: none;
+		}
+
+</style>
+
+<?php if(isset($lang)) { ?>
+	
+	<?php if($lang == "hi") { ?>
+		
+		<style>
+		  ol li {list-style-type: devanagari;}
+		</style>
+
+	<?php } ?>
+
+	<?php if($lang == "gu") { ?>
+
+		<style>
+		  ol li {list-style-type: gujarati;}
+		</style>
+
+	<?php } ?>
+	
+<?php } ?>
+
+</head>
+<body>
