638b294db9fd1f11120960f941f16c9be1d05340
keshav <gyanmargi@icloud.com>
2023-10-24 14:04:13 +0530

    fixed minor bugs related with word matching

--- script.js
+++ script.js
@@ -10,22 +10,22 @@ async function dictionarysearch(searchWord) {
         .then(response => response.json()) // Parse the JSON response
         .then(dataDict => {
             for (const [key, value] of Object.entries(dataDict)) {
-                if (key.toLowerCase().includes(searchWord)) {
+                if (key.toLowerCase() === searchWord) {
                     const result = [key, value, "key"];
                     console.log("first", result);
                     resolve(result);
                     return;
-                } else if (value["hindi_word"].some(word =>word.toLowerCase().includes(searchWord))) {
+                } else if (value["hindi_word"].some(word =>word.toLowerCase() === searchWord)) {
                     const result = [key, value, "hw"];
                     console.log("second", result);
                     resolve(result);
                     return;
-                } else if (value["english_synonyms"].some(word =>word.toLowerCase().includes(searchWord))) {
+                } else if (value["english_synonyms"].some(word =>word.toLowerCase() === searchWord)) {
                     const result = [key, value, "es"];
                     console.log("third", result);
                     resolve(result);
                     return;
-                } else if (value["hindi_synonyms"][0].toLowerCase().includes(searchWord)) {
+                } else if (value["hindi_synonyms"].some(word =>word.toLowerCase() === searchWord)) {
                     const result = [key, value, "hs"];
                     console.log("fourth", result);
                     resolve(result);
@@ -45,10 +45,11 @@ async function dictionarysearch(searchWord) {
 btn.addEventListener('click', fetchandcreatecolumns)
 
 async function fetchandcreatecolumns(){
-    const result = await dictionarysearch(input.value.toLowerCase());
-    console.log(result);
-    dictionary.innerHTML = `
-                <div class="column_english">
+    const result = await dictionarysearch(input.value.trim().toLowerCase());
+    if (result !== null) {
+        // The result is not null
+        dictionary.innerHTML = `
+            <div class="column_english">
                 <div class="word">
                     <span>${result[0]}</span>
                 </div>
@@ -67,7 +68,29 @@ async function fetchandcreatecolumns(){
                 <div class="property">Hindi Synonyms </div>
                 <div class="property">${result[1]["hindi_synonyms"].join(", ")}</div>  
             </div>
-    `;
-    
+            `;
+    } else {
+            // The result is null
+        dictionary.innerHTML = `
+            <div class="column_english">
+                <div class="word">
+                    <span>${input.value}</span>
+                </div>
+                <div class="property">English description </div>
+                <div class="property">"No matching result found."</div>
+                <div class="property">English Synonyms </div>
+                <div class="property">"No matching result found."</div>
+            </div>
 
+            <div class="column_hindi">
+                <div class="word">
+                    <span>${input.value}</span>
+                </div>
+                <div class="property">Hindi description </div>
+                <div class="property">"No matching result found."</div>
+                <div class="property">Hindi Synonyms </div>
+                <div class="property">"No matching result found."</div>  
+            </div>
+            `;
+        }
 }
